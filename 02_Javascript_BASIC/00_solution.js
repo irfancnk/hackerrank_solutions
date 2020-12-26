d0e2fd3ce7abda5dbaf9c83648496fabc8d1d420
irfancnk <irfankaleli@yandex.com>
2020-12-26 19:18:00 +0300

    Question summaries are added.

--- 02_Javascript_BASIC/00_solution.js
+++ 02_Javascript_BASIC/00_solution.js
@@ -3,33 +3,13 @@ function joinedLogger(level, sep) {
         let textToLog = "";
         for (let i = 0; i < args.length; i++) {
             if (args[i].level >= level) {
-                textToLog += args[i].text + ";";
+                textToLog += args[i].text + sep;
             }
         }
         console.log(textToLog);
     }
 }
 
-messages = [
-    {
-        level: 10,
-        text: "foo"
-    },
-    {
-        level: 20,
-        text: "bar"
-    },
-    {
-        level: 30,
-        text: "baz"
-    }
-];
-
-function inner(...args) {
-    for (let i = 0; i < args.length; i++) {
-        console.log(args[i]);
-    }
-}
 
 
 inner(messages[0], messages[1], messages[2])
